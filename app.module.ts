d9a6260958235fa6369338f8fcdc53897e9ef60e
Ammu Ramakrishna <68512468+rakhi6997@users.noreply.github.com>
2020-11-20 12:01:04 +0530

    Client code Angular Application

---
+++ app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShoppingItemComponent } from './shopping-item/shopping-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShoppingItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
